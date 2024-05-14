eb23c71135340a232b7ceaa1ec02dcb70a848843
polbb <paulbessone@gmail.com>
2024-05-14 12:41:10 +0100

    3rd chart

--- src/scripts/streamlit_xhtml.py
+++ src/scripts/streamlit_xhtml.py
@@ -248,14 +248,12 @@ def streamlit_xhtml(company_number):
             size_max=60,
         )
 
-        tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-        with tab1:
+        # tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+
             # Use the Streamlit theme.
             # This is the default. So you can also omit the theme argument.
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        with tab2:
-            # Use the native Plotly theme.
-            st.plotly_chart(fig, theme=None, use_container_width=True)
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
         
 
 
