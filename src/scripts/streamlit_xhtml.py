4c0ca58e5d006050a9ae4afaf4a18c9b9c99517e
polbb <paulbessone@gmail.com>
2024-05-14 11:47:11 +0100

    brx score title

--- src/scripts/streamlit_xhtml.py
+++ src/scripts/streamlit_xhtml.py
@@ -167,7 +167,7 @@ def streamlit_xhtml(company_number):
     with st.container(border=True):
         # st.header('Business Operations Score')
         st.markdown(
-                f"<span style='color: white; font-size: 60px;'>BRX Score</span>",
+                f"<span style='color: white; font-size: 80px;'>BRX Score</span>",
                 unsafe_allow_html=True)
         
         # st.metric('BOS', 250, delta=30)
