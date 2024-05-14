7dc0fb834ad4d24edbf5a62cab3605534f6a82f8
polbb <paulbessone@gmail.com>
2024-05-14 12:21:44 +0100

    3rd chart

--- src/scripts/streamlit_xhtml.py
+++ src/scripts/streamlit_xhtml.py
@@ -228,6 +228,34 @@ def streamlit_xhtml(company_number):
         # Plot!
         st.plotly_chart(fig, use_container_width=True)
 
+    with st.container(border=True):
+        # st.header('Business Operations Score')
+        st.markdown(
+                f"<span style='color: white; font-size: 60px;'>Char 2</span>",
+                unsafe_allow_html=True)
+        
+
+        df = px.data.gapminder()
+
+        fig = px.scatter(
+            df.query("year==2007"),
+            x="gdpPercap",
+            y="lifeExp",
+            size="pop",
+            color="continent",
+            hover_name="country",
+            log_x=True,
+            size_max=60,
+        )
+
+        tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+        with tab1:
+            # Use the Streamlit theme.
+            # This is the default. So you can also omit the theme argument.
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        with tab2:
+            # Use the native Plotly theme.
+            st.plotly_chart(fig, theme=None, use_container_width=True)
         
 
 
